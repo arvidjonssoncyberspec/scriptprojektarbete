8522cecc25467787ef6c88966e24f2c6dedcb776
arvidjonssoncyberspec <arvidcjonsson@gmail.com>
2026-01-10 12:59:57 +0100

    Got some errors and am still learning so trying some things out

--- secure_password_script.py
+++ secure_password_script.py
@@ -1,42 +1,74 @@
 import secrets
 import string
 
-# Funktion som genererar ett säkert lösenord
+# Funktion: Generera ett säkert lösenord
 
 def generate_secure_password(length=12):
     """
-    Skapar ett säkert slumpmässigt lösenord
-    med:
-    - stora bokstäver
-    - små bokstäver
-    - siffror
-    - specialtecken
+    Skapar ett säkert lösenord som ALLTID innehåller:
+    - minst 1 liten bokstav
+    - minst 1 stor bokstav
+    - minst 1 siffra
+    - minst 1 specialtecken
+
     """
 
-    # Alla tillåtna tecken i lösenordet
-    characters = (
-        string.ascii_lowercase +   # a-z
-        string.ascii_uppercase +   # A-Z
-        string.digits +            # 0-9
-        string.punctuation         # !@#$% osv
-    )
+    if length < 4:
+        raise ValueError("Fel vid generering av lösenord, kontakta administratör.")
 
-    # Bygger lösenordet tecken för tecken
-    password = ''.join(
-        secrets.choice(characters) for _ in range(length)
-    )
+    # Teckengrupper
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    special = string.punctuation
 
+    # Tvinga fram minst ett tecken från varje grupp
+    password_characters = [
+        secrets.choice(lowercase),
+        secrets.choice(uppercase),
+        secrets.choice(digits),
+        secrets.choice(special)
+    ]
+
+    # Ser till att minst ett av olika tecken används
+    all_characters = lowercase + uppercase + digits + special
+
+    # Fyll resten av lösenordet slumpmässigt
+    for _ in range(length - 4):
+        password_characters.append(secrets.choice(all_characters))
+
+    # Blanda tecknen så ordningen blir helt slumpmässig
+    secrets.SystemRandom().shuffle(password_characters)
+
+    # Gör listan till en sträng (från 0,1,2,3 til 0123)
+    password = ''.join(password_characters)
     return password
 
-# Huvudprogram
+# Kör Programmet
 
 def main():
-    # Skapar ett lösenord med minst 12 tecken
-    password = generate_secure_password(12)
 
-    print(password)
+    while True:
+        user_input = input("\nAnge önskad lösenordslängd (minst 10): \n")
 
+        try:
+            length = int(user_input)
 
-# Kör programmet
+            if length < 10:
+                print("\nLängden på lösenordet måste innehålla minst 10 tecken.\n")
+                continue
+
+            # Korrekt input → bryt loopen
+            break
+
+        except ValueError:
+            print("\nFelaktig inmatning. Ange ett heltal.\n")
+
+    # Generera lösenordet
+    password = generate_secure_password(length)
+
+    #Skriver ut lösenordet
+    print(f"Här är ditt säkra lösenord: {password}")
+
 if __name__ == "__main__":
     main()
